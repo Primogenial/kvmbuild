fee215d4e9e8dac58fbd1ce96ec68a98625edf7d
Primogenial <140074523+Primogenial@users.noreply.github.com>
2023-07-20 13:42:29 +0100

    Update kvmbuild.sh

--- kvmbuild.sh
+++ kvmbuild.sh
@@ -2,6 +2,11 @@
 # This is a simple script to help configure a normal RHEL, ROCKY8, Alma Linux 
 # server in to a Virtualization host with Cockpit management
 
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root, please 'sudo su' to root then re-run this script." 1>&2
+   exit 1
+fi
 echo " Starting install process now"
 
 sudo dnf -y update
